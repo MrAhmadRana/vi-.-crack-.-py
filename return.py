b4374452105ff7eb2f2f0cd36729e99df024b3da
Ahmad Rana <rana456ahmad789@gmail.com>
2023-01-27 17:26:31 +0500

    1st project of python made a turtle

--- return.py
+++ return.py
@@ -1,1 +1,14 @@
-def cube(num)
+import turtle
+turtle.bgcolor('black')
+turtle.speed(0)
+turtle.hideturtle()
+Colors = ['pink','red','cyan','blue']
+for i in range(120):
+    for c in Colors:
+        turtle.color(c)
+        turtle.circle(200-i,100)
+        turtle.lt(90)
+        turtle.circle(200-i,100)
+        turtle.rt(60)
+        turtle.end_fill()
+turtle.mainloop()
